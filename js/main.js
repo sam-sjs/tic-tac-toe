5e17bb0fd282ae9c565a62ba592ff423ea515954
Sam Skinner <sam.sjskinner@gmail.com>
2020-11-16 15:30:39 +1100

    implimented listening for clicks and marking with X

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('Let\'s play!');
+
+
+
+
+// $(document).ready(function() {   -------JQUERY CODE BELOW HERE-------
+
+$('div.checkSquares').on('click', function() {
+  $(this).html('X');
+});
+
+
+
+
+
+
+// }); //end of $(document).ready() handler
