1377d2bd1af34bc083dbf53099b7b53bc5a6545a
Sam Skinner <sam.sjskinner@gmail.com>
2020-11-16 20:26:56 +1100

    Game now succssfully checks rows

--- js/main.js
+++ js/main.js
@@ -4,8 +4,34 @@ let playerOne = "";
 let playerTwo = "";
 
 let stateCount = 0;
+let tableSize = 3;
 // $(document).ready(function() {   -------JQUERY CODE BELOW HERE-------
 
+//---Check Victory Conditions---
+const victory = function() {
+  let inARow = [];
+  let correctCount = 1;
+  for(let i = 1; i < tableSize + 1; i++) {
+    for(let j = 1; j < tableSize + 1; j++) {
+      const cellValue = $('#' + i + '_' + j).html();
+      inARow.push(cellValue);
+      if(j > 1 && inARow[j - 1] === inARow[j - 2] && inARow[0] !== "") {
+        correctCount++;
+      };
+    };
+    inARow = [];
+    if(correctCount === tableSize) {
+      console.log('Victory!');
+      break;
+    } else {
+      correctCount = 1;
+    };
+  };
+}
+
+
+
+// ---Set Names---
 $('#playerButton').on('click', function() {
   if(stateCount === 0) {
     playerOne = $('#playerName').val();
@@ -20,19 +46,17 @@ $('#playerButton').on('click', function() {
   }
 });
 
-
-
-
-
-
+//---Play Turns---
 $('div.checkSquares').on('click', function() {
   if(stateCount % 2 === 0) {
     $(this).html('X');
     stateCount++
+    victory();
     $('#setPlayers p').html(`${playerTwo} it's your turn!`);
   } else {
     $(this).html('O');
     stateCount++
+    victory();
     $('#setPlayers p').html(`${playerOne} it's your turn!`);
   }
 });
